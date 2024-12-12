d0df2c3552cba51e14e9a7083b53d264d26bc21a
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-12 16:14:04 +0200

    feat: now determining crops in region

--- src/part1.cpp
+++ src/part1.cpp
@@ -3,9 +3,20 @@
 
 using namespace std;
 
+void backtrack(Crops c, int x, int y) {
+  char crop = c.crops[y][x];
+  if (crop == 'B') {
+    cout << '(' << x << ',' << y << ')';
+    backtrack(c, x + 1, y);
+    backtrack(c, x, y + 1);
+  }
+  else { return; }
+}
+
 int main () {
   Crops c;
   c.print();
 
+  backtrack(c, 0, 1);
   return 0;
 }
